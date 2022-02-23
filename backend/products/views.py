404ee544616c845a53a2e700a454da8c34d0f1ff
maegop <megordon@alu.ucam.edu>
2022-02-23 15:18:26 +0100

    added view post order

--- backend/products/views.py
+++ backend/products/views.py
@@ -38,7 +38,14 @@ class OrderCreateListView(GenericAPIView):
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        pass
+        data = request.data
+
+        serializer = self.serializer_class(data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 class OrderDetailView(GenericAPIView):
 

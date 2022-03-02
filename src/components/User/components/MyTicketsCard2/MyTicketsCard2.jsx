77c464d8fb36a7b45abea8df11892fb71794bd88
Bryan Scalzo <50776347+bryanscalzo97@users.noreply.github.com>
2022-03-02 12:19:34 -0300

    fix pagination

---
+++ src/components/User/components/MyTicketsCard2/MyTicketsCard2.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.css";
+const MyTicketsCard2 = ({ item }) => {
+  return (
+    <div className="container ticketContainer">
+      <div className="row">
+        <div className="container ticketUser">
+          <div className="row ">
+            <div className="col-6">
+              <h3>NAMEEEEE2: {item.name_event}</h3>
+              <p>Description: {item.description}</p>
+              <p>Status: {item.status_event}</p>
+              <p>Date Event: {item.date_event}</p>
+              <p>City: {item.city}</p>
+              <p>Location: {item.location}</p>
+              <img src={item.get_image} alt="" />
+            </div>
+            <div className="col-6 ticketImg"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MyTicketsCard2;

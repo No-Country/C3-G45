ef447437a954d0e3281929c6e173aa432f9b1c1a
Bryan Scalzo <50776347+bryanscalzo97@users.noreply.github.com>
2022-03-09 09:24:45 -0300

    fix cart card

--- src/components/Cart/FullCart.jsx
+++ src/components/Cart/FullCart.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const FullCart = ({ cart, total, handleDeleteItem, handleClearCart, finishBuy }) => {
-
+const FullCart = ({
+  cart,
+  total,
+  handleDeleteItem,
+  handleClearCart,
+  finishBuy,
+}) => {
   return (
     <div className="container cart-section">
       <h1 className="mb-5 mt-5">Your shop cart</h1>
       {cart?.event?.map((e) => {
         return (
           <div key={e.id} className="container card mb-5 p-0">
-            {/* <div className="row g-0">
+            <div className="row g-0">
               <div className="col-4 p-2 d-flex alig-items-center justify-content-center body-ticket">
                 <img
                   src="https://res.cloudinary.com/dxjaruq2p/image/upload/v1/media/events/ticket_latam_owsqvf"
@@ -43,20 +48,6 @@ const FullCart = ({ cart, total, handleDeleteItem, handleClearCart, finishBuy })
                   </div>
                 </div>
               </div>
-            </div> */}
-            <div className="card mb-3" style={{ maxWidth: "540px" }}>
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img src="..." className="img-fluid rounded-start" alt="..." />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         );
@@ -79,7 +70,7 @@ const FullCart = ({ cart, total, handleDeleteItem, handleClearCart, finishBuy })
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FullCart
+export default FullCart;
